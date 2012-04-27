250c6db521494da08c5bd9209869ac73868c253f
Otto Linnemann <linneman@gmx.de>
2012-04-27 20:52:35 +0200

    removed experimental code
    Signed-off-by: Otto Linnemann <linneman@gmx.de>

--- start-tiny.scm
+++ start-tiny.scm
@@ -16,12 +16,3 @@
 ;; load the sample program
 (load "test-params.scm")
 
-
-
-(define-syntax or
-  (syntax-rules ()
-    [(_) #f]
-    [(_ e) e]
-    [(_ e1 e2 e3 ...)
-     (let ([t e1]) (if t t (or e2 e3 ...)))]))
-
